b7c580bc8d4c2bef7feaba46994546af69d03150
Psiko23 <tylerpittman2001@gmail.com>
2024-03-29 01:37:04 -0700

    style updates/preview deploy

--- stacked-and-stored/src/components/ProjectsComponents/projectCard.jsx
+++ stacked-and-stored/src/components/ProjectsComponents/projectCard.jsx
@@ -14,7 +14,7 @@ export const ProjectCard = ({ extraClasses, project }) => {
         <div id={project.id} className={`${extraClasses.bg} h-screen flex flex-col justify-center items-center py-5 relative`}>
             <div className="bg-gray-300 h-64 w-full absolute bottom-52"></div>
             <div className={`${extraClasses.card} w-11/12 flex flex-col justify-evenly items-center relative`}>
-                <div className={`${extraClasses.header} card relative shadow-md w-3/4 flex justify-center items-center`}>
+                <div className={`${extraClasses.header} card relative bg-white shadow-md w-3/4 flex justify-center items-center`}>
                     <h1 className="text-4xl">{project.title}</h1>
                 </div>
                 <div className="bg-gray-400 rounded-sm shadow-xl w-full h-3/5 z-10" style={backgroundStyle}></div>
